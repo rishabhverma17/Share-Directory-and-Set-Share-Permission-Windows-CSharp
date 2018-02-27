220046504d527870dc6094b5265d3f52ef58d08d
Rishabh Verma <8901196+rishabhverma17@users.noreply.github.com>
2018-02-27 21:23:35 +0530

    Class File Upload

---
+++ ClassFileShare.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace Aperta_NodalInstaller
+{
+    class ClassFileShare
+    {
+        public string ShareFolder(string FolderPath, string ShareName, string Description)
+        {
+            string strSharePath = FolderPath;
+            string strShareName = ShareName;
+            string strShareDesc = Description;
+            string msg = string.Empty;
+            try
+            {
+                Directory.CreateDirectory(strSharePath);
+                ManagementClass oManagementClass = new ManagementClass("Win32_Share");
+                ManagementBaseObject inputParameters = oManagementClass.GetMethodParameters("Create");
+                ManagementBaseObject outputParameters;
+                inputParameters["Description"] = strShareDesc;
+                inputParameters["Name"] = strShareName;
+                inputParameters["Path"] = strSharePath;
+                inputParameters["Type"] = 0x0;//disk drive 
+                inputParameters["MaximumAllowed"] = null;
+                inputParameters["Access"] = null;//Make Everyone has full control access
+
+                inputParameters["Password"] = null;
+
+
+                outputParameters = oManagementClass.InvokeMethod("Create", inputParameters, null);//// Invoke the method on the ManagementClass object
+
+                if ((uint)(outputParameters.Properties["ReturnValue"].Value) != 0)
+                {
+                    msg = "There is a problem while sharing the directory.";
+                    throw new Exception("There is a problem while sharing the directory.");
+                }
+                else
+                {
+                    msg = ("Share Folder has been created with the name :" + strShareName);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                msg = (ex.Message.ToString());
+            }
+            return msg;
+        }
+    }
+}
